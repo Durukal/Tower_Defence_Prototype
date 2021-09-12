c46a7d3e6f12d9dbdcdf46bc4383fc9bc2d1d68b
durukal <durukaldiker@gmail.com>
2021-09-12 15:21:35 +0300

    Game assets added, Waypoint class and WaypointEditor class added.

---
+++ Tower_Defence_Prototype/Assets/Scripts/Waypoint/Editor/WaypointEditor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Waypoint))]
+public class WaypointEditor : Editor {
+    private Waypoint Waypoint => target as Waypoint;
+
+    private void OnSceneGUI() {
+        Handles.color = Color.green;
+        for (int index = 0; index < Waypoint.Points.Length; index++) {
+            EditorGUI.BeginChangeCheck();
+            //Create Handles
+            Vector3 currentWaypointPoint = Waypoint.CurrentPosition + Waypoint.Points[index];
+            Vector3 newWaypointPoint = Handles.FreeMoveHandle(currentWaypointPoint, quaternion.identity, 0.7f, new Vector3(0.3f, 0.3f, 0.3f), Handles.SphereHandleCap);
+
+            //Create Text
+            GUIStyle textStyle = new GUIStyle();
+            textStyle.fontStyle = FontStyle.Bold;
+            textStyle.fontSize = 16;
+            textStyle.normal.textColor = Color.yellow;
+            Vector3 textAlignment = Vector3.down * 0.5f + Vector3.right * 0.5f;
+            Handles.Label(Waypoint.CurrentPosition + Waypoint.Points[index] + textAlignment, $"{index + 1}", textStyle);
+            EditorGUI.EndChangeCheck();
+            if (EditorGUI.EndChangeCheck()) {
+                Undo.RecordObject(target, "Free Move Handle");
+                Waypoint.Points[index] = newWaypointPoint - Waypoint.CurrentPosition;
+            }
+        }
+    }
+}
---
+++ Tower_Defence_Prototype/Assets/Scripts/Waypoint/Waypoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+    [SerializeField]
+    private Vector3[] _points;
+
+    public Vector3[] Points => _points;
+    public Vector3 CurrentPosition => _currentPosition;
+
+    private Vector3 _currentPosition;
+    private bool _gameStarted;
+
+    void Start() {
+        _gameStarted = true;
+        _currentPosition = transform.position;
+    }
+    
+    void Update() { }
+
+    private void OnDrawGizmos() {
+        if (!_gameStarted && transform.hasChanged) {
+            _currentPosition = transform.position;
+        }
+
+        for (int index = 0; index < _points.Length; index++) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(_points[index] + _currentPosition, 0.5f);
+            if (index < _points.Length - 1) {
+                Gizmos.color = Color.gray;
+                Gizmos.DrawLine(_points[index] + _currentPosition, _points[index + 1] + _currentPosition);
+            }
+        }
+    }
+}
